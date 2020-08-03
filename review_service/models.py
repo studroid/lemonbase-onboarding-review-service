7673a6df6b43f9f6f4806a64f7d3324a7eb5f664
SJKim <ksjksj0122@gmail.com>
2020-08-04 00:24:31 +0900

    Add relationships to the models

--- review_service/models.py
+++ review_service/models.py
@@ -8,14 +8,14 @@ class Person(models.Model):
     created_at = models.DateTimeField(auto_now=True)
 
 
-class ReviewCycle(models.Model):
-    # TODO: Add creator (1:N relationship to Person)
-    name = models.CharField(max_length=50)
-    # TODO: Add question (1:1 relationship to Question)
-    # TODO: Add reviewee (N:N relationship to Person)
-    created_at = models.DateTimeField(auto_now_add=True)
-
-
 class Question(models.Model):
     title = models.CharField(max_length=255)
     description = models.CharField(max_length=255)
+
+
+class ReviewCycle(models.Model):
+    creator = models.ForeignKey(Person, on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
+    question = models.OneToOneField(Question, on_delete=models.CASCADE)
+    reviewee = models.ManyToManyField(Person)
+    created_at = models.DateTimeField(auto_now_add=True)
