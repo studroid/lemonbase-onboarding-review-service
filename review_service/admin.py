c01f4118a443cf788f4d0423b7b252a47a23ad66
SJKim <ksjksj0122@gmail.com>
2020-08-04 00:38:33 +0900

    Register models to admin for testing

--- review_service/admin.py
+++ review_service/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from review_service.models import Question, Person, ReviewCycle
+
+admin.site.register(Person)
+admin.site.register(Question)
+admin.site.register(ReviewCycle)
