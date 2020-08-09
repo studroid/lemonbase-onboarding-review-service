45818134d573a069787895c66baa3bf3ce40f27f
SJKim <ksjksj0122@gmail.com>
2020-08-10 01:49:14 +0900

    Add Django authentication system with email as a username

--- review_service/models.py
+++ review_service/models.py
@@ -1,11 +1,43 @@
+from django.contrib.auth.base_user import AbstractBaseUser, BaseUserManager
 from django.db import models
 
 
-class Person(models.Model):
-    email = models.EmailField()
-    password = models.CharField(max_length=100)
+class PersonManager(BaseUserManager):
+    def create_user(self, email, name, password=None):
+        if not email:
+            raise ValueError('Users must have an email address')
+
+        user = self.model(
+            email=self.normalize_email(email),
+            name=name,
+        )
+
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+
+class Person(AbstractBaseUser):
+    email = models.EmailField(
+        verbose_name="email address",
+        max_length=255,
+        unique=True
+    )
     name = models.CharField(max_length=100)
     created_at = models.DateTimeField(auto_now=True)
+
+    objects = PersonManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['name']
+
+    def __str__(self):
+        return self.email
+
+    def has_perm(self, obj=None):
+        "Consider use this for limiting access to the review policy"
+        # TODO: Only return true if the policy is made by the person
+        return True
 
 
 class Question(models.Model):
