bb051942c12553c971dca6ed2ba049d27e46c95d
SJKim <ksjksj0122@gmail.com>
2020-08-11 01:56:23 +0900

    Add API skeleton codes

---
+++ review_service/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+
+from . import views
+
+app_name = 'review_service'
+urlpatterns = [
+    path('account/sign_up/', views.sign_up, name='account_sign_up'),
+    path('account/sign_in/', views.sign_in, name='account_sign_in'),
+    path('account/sign_out/', views.sign_out, name='account_sign_out'),
+
+    path('policy/', views.PolicyAPI.as_view(), name='policy'),
+]
--- review_service/views.py
+++ review_service/views.py
@@ -1,3 +1,36 @@
+from django.http import JsonResponse
 from django.shortcuts import render
+from django.views import View
 
-# Create your views here.
+
+def sign_up(request):
+    dummy_data = {'request': "SIGN UP"}
+    return JsonResponse(dummy_data)
+
+
+def sign_in(request):
+    dummy_data = {'request': "SIGN IN"}
+    return JsonResponse(dummy_data)
+
+
+def sign_out(request):
+    dummy_data = {'request': "SIGN OUT"}
+    return JsonResponse(dummy_data)
+
+
+class PolicyAPI(View):
+    def get(self, request):
+        dummy_data = {'request': "PolicyAPI GET"}
+        return JsonResponse(dummy_data)
+
+    def post(self, request):
+        dummy_data = {'request': "PolicyAPI POST"}
+        return JsonResponse(dummy_data)
+
+    def put(self, request):
+        dummy_data = {'request': "PolicyAPI PUT"}
+        return JsonResponse(dummy_data)
+
+    def delete(self, request):
+        dummy_data = {'request': "PolicyAPI DELETE"}
+        return JsonResponse(dummy_data)
