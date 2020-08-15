efc57043ba5602a8fce16dabf4d040c1ff6d7643
SJKim <ksjksj0122@gmail.com>
2020-08-16 00:22:00 +0900

    Add some skeleton code test cases

--- review_service/tests.py
+++ review_service/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+
+class PolicyAPIViewTest(TestCase):
+    def test_skeleton_sign_up(self):
+        response = self.client.get(reverse('review_service:account_sign_up'))
+        self.assertContains(response, "SIGN UP")
+
+    def test_skeleton_policy_post(self):
+        response = self.client.post(reverse("review_service:policy"))
+        self.assertContains(response, "POST")
+
