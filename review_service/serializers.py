b55f21d89279271353ce874134b4c5e489915dcd
SJKim <ksjksj0122@gmail.com>
2020-08-27 00:02:55 +0900

    Change Serializer example to use ModelSerializer

--- review_service/serializers.py
+++ review_service/serializers.py
@@ -1,18 +1,13 @@
+from typing import List
+
 from rest_framework import serializers
 
 from review_service.models import Person
 
 
-class PersonSerializer(serializers.Serializer):
-    email = serializers.EmailField(required=True, max_length=255)
-    name = serializers.CharField(max_length=100)
-    created_at = serializers.DateTimeField(read_only=True)
+class PersonSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Person
+        fields: List[str] = ['email', 'name', 'created_at']
 
-    def create(self, validated_data):
-        return Person.objects.create(**validated_data)
 
-    def update(self, instance, validated_data):
-        instance.email = validated_data.get('email', instance.email)
-        instance.name = validated_data.get('name', instance.name)
-        instance.save()
-        return instance
